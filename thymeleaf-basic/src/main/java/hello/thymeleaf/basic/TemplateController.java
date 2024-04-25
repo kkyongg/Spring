c7580d4d67437ac0ae7d66fa76ec8d1b871f47e6
kkyongg <kkyongg2@gmail.com>
2024-04-25 21:29:03 +0900

    [스프링 MVC 2편 - 백엔드 웹 개발 활용 기술] 타임리프 - 기본 기능

---
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,27 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    //템플릿 조각
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+
+    //템플릿 레이아웃
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+}
