8d0cd4870037cba5fa0368003c620aefe854ef27
kkyongg <kkyongg2@gmail.com>
2024-09-08 22:45:26 +0900

    rename

---
+++ servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//HTTP 요청 데이터 - API 메시지 바디 - JSON
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
